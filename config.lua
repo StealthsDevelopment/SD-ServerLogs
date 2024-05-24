fd27399e4c9add2a72b0b8aaaf156e3cb5665eea
Stealth Development <stealthm8development@gmail.com>
2024-05-24 08:28:18 -0500

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,10 +1,14 @@
-config = {
-    JoinWebhook = "",
-    LeaveWebhook = "",
-    ChatMsgWebhook = "",
-    DeathWebhook = "",
-    ResourceWebhook = "",
-    DamageWebhook = "",
-    ShootingWebhook = "",
-    NameChangeWebhook = "",   
-}
+-- config.lua
+
+Config = {}
+
+-- Replace with your actual Discord webhook URL
+Config.WebhookURL = 'https://discord.com/api/webhooks/1243332947805732946/8pu651rgLe4k0XXXsd98Yfa_M94afK_x_uwc64oZMXXWJZ_3B9GCvrb86IES2C2I1Ebe'
+Config.WebhookURL = JoinWebhook = ''
+Config.WebhookURL = LeaveWebhook = ''
+Config.WebhookURL = ChatMsgWebhook = ''
+Config.WebhookURL = DeathWebhook = ''
+Config.WebhookURL = ResourceWebhook = ''
+Config.WebhookURL = DamageWebhook = ''
+Config.WebhookURL = ShootingWebhook = ''
+Config.WebhookURL = NameChangeWebhook = ''   
